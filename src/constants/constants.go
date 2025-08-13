afce3c53814095b2af76ce8b4b53365cce230ccc
Ali Elmi <ali.elmi77@yahoo.com>
2025-08-13 21:08:42 +0330

    Add common utility functions and constants for the project

---
+++ src/constants/constants.go
@@ -0,0 +1,15 @@
+package constants
+
+const (
+	// Claims
+	AuthorizationHeaderKey string = "Authorization"
+	UserIdKey              string = "UserId"
+	FirstNameKey           string = "FirstName"
+	LastNameKey            string = "LastName"
+	UsernameKey            string = "Username"
+	EmailKey               string = "Email"
+	MobileNumberKey        string = "MobileNumber"
+	ExpireTimeKey          string = "Exp"
+
+	RefreshTokenCookieName string = "refresh_token"
+)
