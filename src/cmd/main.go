aa2a1af940bfb68478e1cd02b9b9682c1413c004
Ali Elmi <ali.elmi77@yahoo.com>
2025-08-13 21:36:10 +0330

    feat: add CORS middleware and rate limiting functionality
    - Implemented CORS middleware to handle cross-origin requests with configurable origins.
    - Added rate limiting middleware using tollbooth to restrict the number of requests.
    - Generated Swagger documentation for the API.
    - Updated go.sum with new dependencies for middleware and documentation generation.

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,4 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/alielmi98/image-processing-service/constants"
+	"github.com/alielmi98/image-processing-service/docs"
+	"github.com/alielmi98/image-processing-service/internal/middlewares"
+	"github.com/alielmi98/image-processing-service/pkg/config"
+	"github.com/alielmi98/image-processing-service/pkg/db"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @securityDefinitions.apikey AuthBearer
+// @in header
+// @name Authorization
 func main() {
+	cfg := config.GetConfig()
+
+	err := db.InitDb(cfg)
+	defer db.CloseDb()
+	if err != nil {
+		log.Fatalf("caller:%s  Level:%s  Msg:%s", constants.Postgres, constants.Startup, err.Error())
+	}
+
+	InitServer(cfg)
+
+}
+
+func InitServer(cfg *config.Config) {
+	r := gin.New()
+
+	r.Use(middlewares.Cors(cfg), middlewares.LimitByRequest())
+	RegisterRoutes(r, cfg)
+	RegisterSwagger(r, cfg)
+	log.Printf("Caller:%s Level:%s Msg:%s", constants.General, constants.Startup, "Started")
+	r.Run(fmt.Sprintf(":%s", cfg.Server.InternalPort))
+
+}
+
+func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
+
+}
+func RegisterSwagger(r *gin.Engine, cfg *config.Config) {
+	docs.SwaggerInfo.Title = "golang web api"
+	docs.SwaggerInfo.Description = "This is a sample server for golang web api"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/api"
+	docs.SwaggerInfo.Host = fmt.Sprintf("localhost:%s", cfg.Server.InternalPort)
+	docs.SwaggerInfo.Schemes = []string{"http"}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
